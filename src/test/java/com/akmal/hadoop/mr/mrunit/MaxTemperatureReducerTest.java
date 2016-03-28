306a8ff6b7ea9dc4ea8e9428886a5952b323c217
Akmal Rakhimov <arakhimoff@gmail.com>
2016-03-28 14:52:27 +0900

    Coded MaxTemperatureReducer and MaxTemperatureReducerTest

---
+++ src/test/java/com/akmal/hadoop/mr/mrunit/MaxTemperatureReducerTest.java
@@ -0,0 +1,23 @@
+package com.akmal.hadoop.mr.mrunit;
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.*;
+
+import com.akmal.hadoop.mr.MaxTemperatureReducer;
+
+public class MaxTemperatureReducerTest {
+	
+	@Test
+	public void returnsMaximumIntegerInValues() throws IOException,
+			InterruptedException {
+		new ReduceDriver<Text, IntWritable, Text, IntWritable>()
+			.withReducer(new MaxTemperatureReducer())
+			.withInput(new Text("1950"),
+					Arrays.asList(new IntWritable(10), new IntWritable(5)))
+			.withOutput(new Text("1950"), new IntWritable(10))
+			.run();
+	}
+}
